720678398faa47838723f341d5ae992fdb1f49ce
NekoSaiKou <ricky0711.yang@gmail.com>
2020-07-19 16:48:19 +0800

    Feat: Upload simple 3axis planar robot

---
+++ 3axis_planar_robot/arm.py
@@ -0,0 +1,77 @@
+"""scara class"""
+import json
+import numpy as np
+from math import sin, cos, pi
+
+class Scara():
+    """ Scara object """
+    def __init__(self):
+        """ Initialize robot current state """
+        with open('config.json', 'r') as file:
+            cfg_dict = json.load(fp=file)
+
+            """
+            Scara initial parameter. In this arm, only 2D is in the game. Z value is always 1.
+            Beside, this is a three joint scara robot.
+            """
+            self.joint_a = np.array(cfg_dict["joint_a"]) * pi / 180.0
+            self.joint_d = np.array(cfg_dict["joint_d"])
+            self.link_a = np.array(cfg_dict["link_a"]) * pi / 180.0
+            self.link_d = np.array(cfg_dict["link_d"])
+            self.speed_a = np.array(cfg_dict["speed"])
+            self.increment_a = 0.001 * 2 * pi/self.speed_a
+
+    def __f_kinematics(self):
+        self.t01 = np.array([[cos(self.joint_a[0]), -1*sin(self.joint_a[0]), 0, cos(self.joint_a[0])*self.link_d[0]],
+                             [sin(self.joint_a[0]),  1*cos(self.joint_a[0]), 0, sin(self.joint_a[0])*self.link_d[0]],
+                             [0                   ,  0                     , 1, 0                                  ],
+                             [0                   ,  0                     , 0, 1                                  ]])
+        self.t12 = np.array([[cos(self.joint_a[1]), -1*sin(self.joint_a[1]), 0, cos(self.joint_a[1])*self.link_d[1]],
+                             [sin(self.joint_a[1]),  1*cos(self.joint_a[1]), 0, sin(self.joint_a[1])*self.link_d[1]],
+                             [0                   ,  0                     , 1, 0                                  ],
+                             [0                   ,  0                     , 0, 1                                  ]])
+        self.t23 = np.array([[cos(self.joint_a[2]), -1*sin(self.joint_a[2]), 0, cos(self.joint_a[2])*self.link_d[2]],
+                             [sin(self.joint_a[2]),  1*cos(self.joint_a[2]), 0, sin(self.joint_a[2])*self.link_d[2]],
+                             [0                   ,  0                     , 1, 0                                  ],
+                             [0                   ,  0                     , 0, 1                                  ]])
+        
+        self.t02 = np.dot(self.t01, self.t12)
+        self.t03 = np.dot(self.t02, self.t23)
+
+        self.theta_123 = np.sum(self.joint_a[0:3])
+        self.theta_12  = np.sum(self.joint_a[0:2])
+        self.theta_1   = np.sum(self.joint_a[0:1])
+        # self.t02 = np.array([[cos(theta_12), -1*sin(theta_12), 0, cos(theta_1)*self.link_d[0] + cos(theta_12)*self.link_d[1]],
+        #                      [sin(theta_12),  1*cos(theta_12), 0, sin(theta_1)*self.link_d[0] + sin(theta_12)*self.link_d[1]],
+        #                      [0            ,  0              , 1, 0                                  ],
+        #                      [0            ,  0              , 0, 1                                  ]])
+
+        # self.t03 = np.array([[cos(theta_123), -1*sin(theta_123), 0, cos(theta_1)*self.link_d[0] + cos(theta_12)*self.link_d[1] + cos(theta_123)*self.link_d[2]],
+        #                      [sin(theta_123),  1*cos(theta_123), 0, sin(theta_1)*self.link_d[0] + sin(theta_12)*self.link_d[1] + sin(theta_123)*self.link_d[2]],
+        #                      [0             ,  0               , 1, 0                                  ],
+        #                      [0             ,  0               , 0, 1                                  ]])
+
+        self.__update_jointpos(self.t01, self.t02, self.t03)
+
+    def __update_jointpos(self, t01, t02, t03):
+        """ Update keyposition data """
+        self.keypos = []
+        self.keypos.append(np.array([0,0,0]))
+        self.keypos.append(t01[0:3, 3])
+        self.keypos.append(t02[0:3, 3])
+        self.keypos.append(t03[0:3, 3])
+    
+    def get_keypos(self):
+        """ Get Scara joint positions in order to plot the arm """
+        return self.keypos
+    
+    def set_velocity(self, id, time):
+        self.speed_a[id] = time
+        self.increment_a = 0.001 * 2 * pi/time
+
+    def update_angle(self, direction):
+        self.joint_a = self.joint_a + self.increment_a * direction
+        print(self.joint_a)
+
+    def update(self):
+        self.__f_kinematics()
---
+++ 3axis_planar_robot/main.py
@@ -0,0 +1,61 @@
+"""Simple scara straight line trajectory simulator"""
+
+import arm
+import cv2
+import numpy as np
+from math import hypot, pi, sin, cos
+
+if __name__ == "__main__":
+    robot = arm.Scara()
+    robot.update()
+
+    center = np.array([320,320])
+    clockwise = 1
+    cv2.namedWindow("test", cv2.WINDOW_NORMAL)
+    while True:
+        # Blank frame
+        sim_frame = np.zeros((640,640,3))
+
+        # Calculate Jacobian
+        a0_s1   = robot.link_d[0] * sin(robot.theta_1)
+        a0_c1   = robot.link_d[0] * cos(robot.theta_1)
+        a1_s12  = robot.link_d[1] * sin(robot.theta_12)
+        a1_c12  = robot.link_d[1] * cos(robot.theta_12)
+        a2_s123 = robot.link_d[2] * sin(robot.theta_123)
+        a2_c123 = robot.link_d[2] * cos(robot.theta_123)
+
+        J = np.array([[-1*a0_s1-a1_s12-a2_s123, -1*a1_s12-a2_s123, -1*a2_s123],
+                      [ 1*a0_c1+a1_c12+a2_c123,  1*a1_c12+a2_c123,  1*a2_c123],
+                      [ 1                     ,  1                , 1]])
+
+        # Velocity of end effector in this case is x:-500,y:0, theta: 0
+        vel = np.dot(np.linalg.inv(J),np.array([-500,0,0]))
+
+        # Update angular velocity
+        robot.increment_a = 0.001 * vel
+
+        # Update angle
+        robot.update_angle(np.array([1 * clockwise, 1 * clockwise, 1 * clockwise]))
+
+        # Update Robot
+        robot.update()
+
+        # Get command and Send to Robot
+        keypos = robot.get_keypos()
+
+        # Plot planar robot
+        for i in range(1,len(keypos)):
+            joint_f = (keypos[i-1][0:2]*[1, -1] + center).astype(int)
+            joint_e = (keypos[i][0:2]*[1, -1] + center).astype(int)
+            joint_f = tuple(joint_f)
+            joint_e = tuple(joint_e)
+            cv2.line(sim_frame, joint_f, joint_e, (0, 255, 0), 5)
+            cv2.circle(sim_frame, joint_f, 10, (0, 0, 255), -1)
+        cv2.imshow("test", sim_frame)
+        key = cv2.waitKey(1)
+
+        # q: quit, r: change direction
+        if key & 0xFF == ord('q'):
+            break
+        elif key & 0xFF == ord('r'):
+            clockwise = -1 * clockwise
